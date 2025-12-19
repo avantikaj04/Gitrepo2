b05c93d974fba0851e047f0194c49b02576a36ba
acs <acs@DESKTOP-4CPLF63>
2025-12-19 12:24:20 +0530

    intial commit

---
+++ Spring1/src/main/java/com/app/controller/Homecontoller.java
@@ -0,0 +1,26 @@
+package com.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+
+@Controller
+public class Homecontoller 
+{
+	@RequestMapping("/")
+	public String defaultpage()
+	{
+		return "index";
+	}
+	@RequestMapping("hello")
+	public String preWelcome()
+	{
+		return "welcome";
+	}
+	
+	
+	
+	
+}
